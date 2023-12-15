6bdd1479482aae4007ac027aa43c4b04dde6a883
yurilavinas <yclavinas@gmail.com>
2023-12-15 16:09:10 +0100

    active learning for gecco

--- scripts/al_random_nmodels_10.sh
+++ scripts/al_random_nmodels_10.sh
@@ -1,3 +1,3 @@
 
-python al_sampling_mu.py ../configs/random_gen_10_nmodels_10.yml $1
-python al_sampling_oneplus.py ../configs/random_gen_10_nmodels_10.yml $1
+python al_sampling_mu.py ../configs/config_random_gen_10_nmodels_10.yml $1
+python al_sampling_oneplus.py ../configs/config_random_gen_10_nmodels_10.yml $1
--- scripts/al_uncertanty_nmodels_15.sh
+++ scripts/al_uncertanty_nmodels_15.sh
@@ -1,3 +1,3 @@
 
-python al_sampling_mu.py ../configs/uncertanty_gen_10_nmodels_15.yml $1
-python al_sampling_oneplus.py ../configs/uncertanty_gen_10_nmodels_15.yml $1
+python al_sampling_mu.py ../configs/config_uncertanty_gen_10_nmodels_15.yml $1
+python al_sampling_oneplus.py ../configs/config_uncertanty_gen_10_nmodels_15.yml $1
