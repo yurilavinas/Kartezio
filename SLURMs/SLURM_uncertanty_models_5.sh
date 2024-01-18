d1487b2300a159a9d28d2155f5d0fb7c4704d045
yurilavinas <yclavinas@gmail.com>
2024-01-18 17:55:55 +0100

    uncertainty

--- SLURMs/SLURM_uncertanty_models_5.sh
+++ SLURMs/SLURM_uncertanty_models_5.sh
@@ -15,7 +15,6 @@ export PATH="/users/p16043/lavinas/miniconda3/bin:$PATH"
 module purge
 module avail python
 module load python/3.8.18
-conda activate cgp_AL
 module load intelmpi chdb/1.0
 
 # RUN CHDB
