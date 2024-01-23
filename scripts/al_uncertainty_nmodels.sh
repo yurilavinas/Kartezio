db2057aec7b66aa62857e59fcba4fad74e02ca77
yurilavinas <yclavinas@gmail.com>
2024-01-23 14:09:55 +0100

    cgp + U + uw + rnd

---
+++ scripts/al_uncertainty_nmodels.sh
@@ -0,0 +1,5 @@
+
+python ../al_sampling_oneplus.py ../configs/config_uncertainty_gen_100_nmodels_2.yml $1
+python ../al_sampling_oneplus.py ../configs/config_uncertainty_gen_100_nmodels_5.yml $1
+python ../al_sampling_oneplus.py ../configs/config_uncertainty_gen_100_nmodels_10.yml $1
+python ../al_sampling_oneplus.py ../configs/config_uncertainty_gen_100_nmodels_15.yml $1
