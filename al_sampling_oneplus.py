d9e88a14195cfd5b3643424fc8cacacbf256cb36
yurilavinas <yclavinas@gmail.com>
2024-01-23 14:59:09 +0100

    cgp + U + uw + rnd

--- al_sampling_oneplus.py
+++ al_sampling_oneplus.py
@@ -204,7 +204,7 @@ if __name__ == "__main__":
             # AL - end
         
         # saving information for future analysis
-        _id = np.min(fitness)
+        _id = fitness.index(min(fitness))
         active_nodes = models[0].parser.parse_to_graphs(elites[_id])
         data = [run, (gen+1), eval_cost, np.min(test_fits), np.min(fitness), test_best_ever, len(active_nodes[0]+active_nodes[1]), idx]
         with open(file_ensemble, 'a') as f:
