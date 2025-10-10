a2041f5b004a0398e0f7baee10dd830666285bef
Yuri Lavinas <yclavinas@gmail.com>
2025-10-10 17:00:51 +0200

    typical onepluslambda original

--- al_sampling_oneplus.py
+++ al_sampling_oneplus.py
@@ -338,8 +338,7 @@ if __name__ == "__main__":
 
     DATASET = framework["DATASET"]  
     RESULTS = framework["save_results"]+"_oneplus_nMut_"+str(n_mutations)+"_nDiv_"+str(n_diverse)
-    print(RESULTS)
-    e()
+
     generations = config["generations"]
     
     CHANNELS = [1, 2]
